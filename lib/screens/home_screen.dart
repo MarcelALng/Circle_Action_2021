fe045f8ce1af0a2a84e6bbc2fc3378ff20343d18
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-04-21 18:12:34 +0200

    [Screen Controller Animation] Add Animation controller Fade transition works fine OK

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,11 +7,36 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin // animation controller
+{
+  AnimationController _animationController;
+  Animation _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 2000));
+    _animation = CurvedAnimation(
+        parent: _animationController, curve: Curves.bounceInOut);
+    // as AnimationController; /*quick fix added by suggestion*/
+    _animationController.forward(); // to launch Animation controller
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: FadeTransition(
+        opacity: _animationController,
+        child: Container(
+          alignment: Alignment.center,
+          child: Text(
+            ("Action!"),
+            style: TextStyle(fontSize: 45.0, color: Colors.black),
+          ),
+        ),
+      ),
     );
   }
 }
