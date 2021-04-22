350060f2fbbe0e90d898905f62cb07d580651e9c
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-04-22 08:22:47 +0200

    [Controller] add algo to calculate 6 <turn nulber <9 OK

--- lib/controllers/action_controller.dart
+++ lib/controllers/action_controller.dart
@@ -1,10 +1,50 @@
 import 'dart:math';
 
 class ActionController {
+  String _decodeColor(int color) {
+    switch (color) {
+      case 0:
+        return "vert";
+        break;
+      case 1:
+        return "bleu clair";
+        break;
+      case 2:
+        return "violet";
+        break;
+      case 3:
+        return "rose";
+        break;
+      case 4:
+        return "jaune";
+        break;
+      case 5:
+        return "bleu foncé";
+        break;
+      case 6:
+        return "rouge";
+        break;
+      case 7:
+        return "orange";
+        break;
+      default:
+        return "erreur";
+        break;
+    }
+  }
+
   getRandomPosition() {
-    int test = Random().nextInt(9);
-    double testDouble = Random().nextDouble();
-    print(test);
-    print(testDouble);
+    int minTurns = 6;
+    int maxTurns = 10;
+    int colorChoice = Random().nextInt(8);
+    double colorAlgo = (1.0 / 8.0) * colorChoice;
+    print("la couleur est ${_decodeColor(colorChoice)}");
+
+    double position = (Random().nextInt(maxTurns - minTurns) + minTurns)
+        //calculate turns number
+        +
+        colorAlgo;
+    print(position);
+    return position;
   }
 }
